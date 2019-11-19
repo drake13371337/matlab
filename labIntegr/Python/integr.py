338f7c8709ab8f3497018c8efee6c38fa7da9392
drake13371337 <48164943+drake13371337@users.noreply.github.com>
2019-11-19 21:31:50 +0300

    Add files via upload

--- labIntegr/Python/integr.py
+++ labIntegr/Python/integr.py
@@ -8,125 +8,99 @@ def trapeze(a, b, e):
     buff = 0
     while 1:
         h = (b-a)/n
-        h1 = h/2
-        for i in range(n-1):
+        for i in range(1, n-1):
             res = res+func(a+h*i)
-        for i in range(n*2-1):
-            buff = buff+func(a+h1*i)
         res = (res*2+func(a)+func(b))*h/2
-        buff = (buff*2+func(a)+func(b))*h1/2
         if abs(buff-res) <= (e*3):
-            print("Trapeze: res = ", res, ", n = ", n)
+            print("Trapeze: res = ", res, ", n = ", n, ", e = ", e)
             return 0
+        buff = res
         res = 0
-        buff = 0
         n += 1
 
 
 def rectangle(a, b, e):
-    n = 1
+    n = 2
     trg = 1
     res = 0
     buff = 0
     while trg:
         h = (b-a)/n
-        h1 = h/2
 
-        for i in range(0, n-1):
+        for i in range(n-1):
             res = res+func(a+h*i)
-        for i in range(0, n*2-1):
-            buff = buff+func(a+h1*i)
 
         res = res*h
-        buff = buff*h1
 
         if abs(buff-res) <= (e*3):
-            print("Rectangle(left): res = ", res, ", n = ", n)
+            print("Rectangle(left): res = ", res, ", n = ", n, ", e = ", e)
             n = 1
             trg = 0
         else:
             n += 1
+        buff = res
         res = 0
-        buff = 0
     trg = 1
     while trg:
         h = (b-a)/n
-        h1 = h/2
 
-        for i in range(n):
-            res = res+func(a+h*i);
-        for i in range(n*2):
-            buff = buff+func(a+h1*i)
+        for i in range(1, n):
+            res = res+func(a+h*i)
 
         res = res*h
-        buff = buff*h1
 
         if abs(buff-res) <= (e*3):
-            print("Rectangle(right): res = ", res, ", n = ", n)
+            print("Rectangle(right): res = ", res, ", n = ", n, ", e = ", e)
             trg = 0
             n = 1
-        else: n += 1
+        else:
+            n += 1
+        buff = res
         res = 0
-        buff = 0
     trg = 1
     while trg:
         h = (b-a)/n
-        h1 = h/2
 
-        for i in range(n-1):
+        for i in range(1, n-1):
             res = res+func(a+h*i)
-        for i in range(n*2-1):
-            buff = buff+func(a+h1*i)
 
         res = (res+(func(a)+func(b))/2)*h
-        buff = (buff+(func(a)+func(b))/2)*h1
 
         if abs(buff-res) <= (e*3):
-            print("Rectangle(middle): res = ", res, ", n = ", n)
+            print("Rectangle(middle): res = ", res, ", n = ", n, ", e = ", e)
             trg = 0
-        else: n += 1
+        else:
+            n += 1
+        buff = res
         res = 0
-        buff = 0
     return 0
 
 
 def simpson(a, b, e):
-    n = 1
+    n = 2
     res = 0
     buff = 0
     res1 = 0
-    buff1 = 0
 
     while 1:
         h = (b-a)/n/2
-        h1 = h/2
 
-        for i in range(n):
+        for i in range(1, n):
             res = res+func(a+h*(2*i-1))
         res = res*4
-        for i in range(n-1):
+        for i in range(1, n-1):
             res1 = res1+func(a+h*2*i)
         res1 = res1*2
         res = res+res1
 
-        for i in range(n*2):
-            buff = buff+func(a+h1*(2*i-1))
-        buff = buff*4
-        for i in range(n*2-1):
-            buff1 = buff1+func(a+h1*2*i)
-        buff1 = buff1*2
-        buff = buff+buff1
-
         res = (res+func(a)+func(b))*h/3
-        buff = (buff+func(a)+func(b))*h1/3
 
         if abs(buff-res) <= (e*15):
-            print("Simpson: res = ", res, ", n = ", n)
+            print("Simpson: res = ", res, ", n = ", n, ", e = ", e)
             return 0
+        buff = res
         res = 0
-        buff = 0
         res1 = 0
-        buff1 = 0
         n = n+2
 
 
@@ -134,7 +108,12 @@ if __name__ == "__main__":
     a = 0.6
     b = 1.724
     e = 0.0001
+    e1 = 0.00001
 
     trapeze(a, b, e)
     rectangle(a, b, e)
     simpson(a, b, e)
+    print(" ")
+    trapeze(a, b, e1)
+    rectangle(a, b, e1)
+    simpson(a, b, e1)
