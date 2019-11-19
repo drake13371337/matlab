9d2374f9f3475c748b6bcf86e156173533e66774
drake13371337 <48164943+drake13371337@users.noreply.github.com>
2019-11-19 18:41:10 +0300

    Add files via upload

--- labIntegr/C++/main.cpp
+++ labIntegr/C++/main.cpp
@@ -23,8 +23,7 @@ int main(){
 }
 
 double func(double x){
-    double y = sqrt(x+pow(x, 3));
-    return y;
+    return sqrt(x+pow(x, 3));
 }
 
 double trapeze(double a, double b, double e){
@@ -92,6 +91,27 @@ double rectangle(double a, double b, double e){
         if(abs(buff-res)<=(e*3)){
             cout<<"Rectangle(right): res = "<<setprecision(8)<<res<<", n = "<<n<<endl;
             trg=0;
+            n=1;
+        } else n++;
+        res=0;
+        buff=0;
+    }
+    trg=1;
+    while(trg){
+        h=(b-a)/n;
+        h1=h/2;
+
+        for(int i=1; i<n-1; i++)
+            res=res+func(a+h*i);
+        for(int i=1; i<n*2-1; i++)
+            buff=buff+func(a+h1*i);
+
+        res=(res+(func(a)+func(b))/2)*h;
+        buff=(buff+(func(a)+func(b))/2)*h1;
+
+        if(abs(buff-res)<=(e*3)){
+            cout<<"Rectangle(middle): res = "<<setprecision(8)<<res<<", n = "<<n<<endl;
+            trg=0;
         } else n++;
         res=0;
         buff=0;
@@ -101,27 +121,28 @@ double rectangle(double a, double b, double e){
 
 double simpson(double a, double b, double e){
     double res, res1, h, buff, buff1, h1;
-    int n=2;
+    int n=1;
     res=0;
     buff=0;
     res1=0;
     buff1=0;
+
     while(true){
-        h=(b-a)/n;
+        h=(b-a)/n/2;
         h1=h/2;
 
-        for(int i=1; i<n/2; i++)
+        for(int i=1; i<n; i++)
             res=res+func(a+h*(2*i-1));
         res=res*4;
-        for(int i=1; i<n/2-1; i++)
+        for(int i=1; i<n-1; i++)
             res1=res1+func(a+h*2*i);
         res1=res1*2;
         res=res+res1;
 
-        for(int i=1; i<n; i++)
+        for(int i=1; i<n*2; i++)
             buff=buff+func(a+h1*(2*i-1));
         buff=buff*4;
-        for(int i=1; i<n-1; i++)
+        for(int i=1; i<n*2-1; i++)
             buff1=buff1+func(a+h1*2*i);
         buff1=buff1*2;
         buff=buff+buff1;
